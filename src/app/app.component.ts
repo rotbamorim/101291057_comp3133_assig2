8b1da8a8633741cba87eb097738b91500c1c7190
Raphael <rotbamorim@gmail.com>
2023-04-08 16:36:02 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '101291057_comp3133_assig2';
+}
